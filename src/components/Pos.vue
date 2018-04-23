756b2583a0dc59e9cf6f9b336fdb9a6c82341494
Swift <swift@SwiftdeMacBook-Air.local>
2018-04-23 22:27:16 +0800

    Initial commit

---
+++ src/components/Pos.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Hello AwesomePOS Demo</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'data'
+    }
+  }
+}
+</script>
